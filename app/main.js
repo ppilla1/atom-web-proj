6ba4f2f2b4bb01398081f12ec30b8c1ea5f96ced
ppilla1 <informprashant@gmail.com>
2017-05-13 20:45:55 +0530

    Example for Class inheritance using prototype and ES6 class

--- app/main.js
+++ app/main.js
@@ -3,19 +3,54 @@
 /* Sample IIFE */
 (function(){
 
+  // Pre ES6 Class inheritance through prototype chaining
+  function Animal(voice) {
+    this.voice = voice || "Grunt";
+  }
+
+  Object.defineProperty(Animal.prototype, "speak", {
+    get : function () {
+      alert(this.voice);
+    }
+  });
+
   function Cat(name, color) {
+    Animal.call(this,"Meeoow!");
     this.name = name;
     this.color = color;
   }
 
-  alert(JSON.stringify(Cat.prototype));
+  Cat.prototype = Object.create(Animal.prototype);
+  Cat.prototype.constructor = Cat;
+  var fluffy = new Cat("Fluffy", "White & Orange");
 
-  var cat = new Cat("Fluffy", "White & Orange");
+  fluffy.speak;
 
-  alert("Cat.prototype = cat.__proto__ : " + (Cat.prototype === cat.__proto__));
+  alert(fluffy.constructor.toString());
+  alert("Fluffy is Cat " + (fluffy instanceof Cat));
+  alert("Fluffy is Animal " + (fluffy instanceof Animal));
 
-  Cat.prototype.age = 3;
+  // Post ES6 Class inheritance using class and extends syntax
 
-  alert(JSON.stringify(Cat.prototype));
-  alert(JSON.stringify(cat.__proto__));
+  class Animal6 {
+    constructor(voice) {
+      this.voice = voice || "Grunt";
+    }
+
+    speak () {
+      alert(this.voice);
+    }
+  }
+
+  class Cat6 extends Animal6{
+    constructor(name, color){
+      super("Meeooww! - 6");
+      this.name = name;
+      this.color = color;
+    }
+  }
+
+  var fluffy6 = new Cat6("Fluffy6", "Off White & Orange!");
+  fluffy6.speak();
+  alert(fluffy6.constructor);
 })();
